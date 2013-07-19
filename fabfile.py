33788c2cece134cf987629f39c8a66ead34e9209
paulm <paulm@freegeek.org>
2013-07-18 17:29:53 -0700

    added git functions

--- fabfile.py
+++ fabfile.py
@@ -15,11 +15,39 @@ class MyTests(unittest.TestCase):
     def test_example(self):
         pass
 
+# DO WE NEED THIS?
 env.hosts = ['bruno']
+
 code_dir=/home/paulm/code/rt_scripts
+repository='rtscripts'
+
 def git_status():
     with cd(code_dir):
-        result = run("git status")
+        result = run("git status -z", capture=True)
+        if len(result) != 0:
+            abort('Aborting...uncommitted changes or files. Please commit any changes to git')
+            return False
+        else
+            return True
+
+def git_push(repo=repository):
+    with cd(code_dir):
+        result = run("git push " + repo)
+        if result.failed:
+            abort('Aborting...unable to push to repository: ' + repo)
+            return False
+        else
+            return True
+
+def copy_file_to_server(destfile='ticket_check'):
+    # TODO scp file to server
+    # specifying defalut server, server ob command line
+
+def deploy:
+    git_status()
+    git_push()
+
+
 
 if __name__ == "__main__":
         unittest.main()
