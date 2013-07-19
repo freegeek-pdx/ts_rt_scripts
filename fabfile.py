71bb5c98a1691087c8c58caf1293b34501d91905
paulm <paulm@freegeek.org>
2013-07-19 13:05:53 -0700

    added deploy()

--- fabfile.py
+++ fabfile.py
@@ -1,54 +1,36 @@
 from __future__ import absolute_import, print_function, unicode_literals
-import unittest
 from fabric.api import *
 from fabric.contrib.console import confirm
-
-
-class MyTests(unittest.TestCase):
-        # pylint: disable=R0904
-
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-
-    def test_example(self):
-        pass
 
 # DO WE NEED THIS?
 env.hosts = ['bruno']
 
-code_dir=/home/paulm/code/rt_scripts
-repository='rtscripts'
+code_dir = '/home/paulm/code/rt_scripts'
+repository = 'rtscripts'
 
 def git_status():
     with cd(code_dir):
-        result = run("git status -z", capture=True)
+        result = local("git status -z", capture=True)
         if len(result) != 0:
             abort('Aborting...uncommitted changes or files. Please commit any changes to git')
-            return False
-        else
-            return True
 
 def git_push(repo=repository):
     with cd(code_dir):
-        result = run("git push " + repo)
+        result = local("git push " + repo)
         if result.failed:
             abort('Aborting...unable to push to repository: ' + repo)
-            return False
-        else
-            return True
 
 def copy_file_to_server(destfile='ticket_check'):
-    # TODO scp file to server
-    # specifying defalut server, server ob command line
+    with cd(code_dir):
+        put(code_dir + destfile, '/usr/local/bin',  use_sudo=True, mirror_local_mode=True)
 
-def deploy:
+
+    # TODO scp file to server
+    # specifying default server, server on command line
+
+def deploy():
     git_status()
     git_push()
+    copy_file_to_server('foo')
 
-
-
-if __name__ == "__main__":
-        unittest.main()
 
