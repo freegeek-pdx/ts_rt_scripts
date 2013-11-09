5b2c40ca782a5bbfe83fe32abcf9a63d587c822c
paulm <paulm@freegeek.org>
2013-11-09 12:32:18 -0800

    added check date, test for raised exception in getcalculated_averages ,removed unused tests

--- test_generate_completion_reports.py
+++ test_generate_completion_reports.py
@@ -32,18 +32,29 @@ class MyTests(unittest.TestCase):
         result = days_ago(date, 7)
         self.assertEquals(result, expected)
 
-    def test_calculate_week(self):
-        date = datetime.date(2013, 11, 01)
-        expected = datetime.date(2013, 10, 25)
-        result = calculate_week(date)
-        self.assertEquals(result, expected)
+    def test_check_date_true(self):
+        start = datetime.date(2013, 10, 25)
+        end = datetime.date(2013, 11, 01)
+        test_date =  datetime.date(2013, 10, 27)
+        self.assertTrue(check_date((start, end), test_date))
+    
+    def test_check_date_false(self):
+        start = datetime.date(2013, 10, 25)
+        end = datetime.date(2013, 11, 01)
+        test_date =  datetime.date(2013, 9, 27)
+        self.assertFalse(check_date((start, end), test_date))
+    
+    def test_check_date_start_is_true(self):
+        start = datetime.date(2013, 10, 25)
+        end = datetime.date(2013, 11, 01)
+        test_date =  datetime.date(2013, 10, 25)
+        self.assertTrue(check_date((start, end), test_date))
 
-    def test_calculate_two_week(self):
-        date = datetime.date(2013, 11, 01)
-        expected = datetime.date(2013, 10, 18)
-        result = calculate_two_week(date)
-        self.assertEquals(result, expected)
-
+    def test_check_date_end_is_true(self):
+        start = datetime.date(2013, 10, 25)
+        end = datetime.date(2013, 11, 01)
+        test_date =  datetime.date(2013, 11, 01)
+        self.assertTrue(check_date((start, end), test_date))
 
     def test_calculate_averages(self):
         (result1, result2) = calculate_averages([1, 2, 3, 4])
@@ -53,6 +64,16 @@ class MyTests(unittest.TestCase):
         date = datetime.date(2013, 11, 01)
         (result1, result2) = get_calculated_averages(date, self.db)
         self.assertEquals((result1, result2), (2.5, 2))
+
+
+    def test_get_calculated_averages_fails(self):
+        date = datetime.date(2013, 11, 11)
+        try:
+            (result1, result2) = get_calculated_averages(date, self.db)
+            raised_exception = False
+        except LocalError: 
+            raised_exception = True
+        self.assertTrue(raised_exception)
 
 
 if __name__ == "__main__":
