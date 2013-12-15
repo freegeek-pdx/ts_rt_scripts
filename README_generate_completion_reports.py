5e26de95e1d8c510b65eeab95417eec3d8456950
paulm <paulm@freegeek.org>
2013-12-14 16:25:08 -0800

    initial commit

---
+++ README_generate_completion_reports.py
@@ -0,0 +1,13 @@
+Dependencies
+Python2.7
+Python Requests
+python-rt https://gitlab.labs.nic.cz/labs/python-rt
+(should have a local copy installed on tsbackup, unntested with newer versions)
+jsondb
+request_tracker 
+these last two are in the freegeek github repostitory
+
+configure EMAIL, WEEKLYDB, MONTHLYDB on installed copy
+
+use in cronjob with -m flag set 
+
